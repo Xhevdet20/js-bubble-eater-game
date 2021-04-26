345dd0d11389b708252bae0ca7f6d375309d34b4
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-04-26 10:53:43 +0200

    part 2

--- script.js
+++ script.js
@@ -12,27 +12,32 @@ ctx.font = '50px Georgia';
 //Mouse Interactivity
 let canvasPosition = canvas.getBoundingClientRect();
 const mouse = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
+  x: canvas.width/2,
+  y: canvas.height/2,
   click: false
 };
 
-canvas.addEventListener('mousedown', function () {
+canvas.addEventListener('mousedown', function (){
   mouse.click = true;
   mouse.x = event.x - canvasPosition.left;
   mouse.y = event.y - canvasPosition.top;
 });
 
-canvas.addEventListener('mouseup', function () {
+canvas.addEventListener('mouseup', function(){
   mouse.click = false;
 })
 
 
 // Player
+const playerLeft = new Image();
+playerLeft.src = 'fish_swim_left.png';
+const playerRight = new Image();
+playerRight.src = 'fish_swim_right.png';
+
 class Player {
-  constructor() {
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
+  constructor(){
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
     this.radius = 50;
     this.angle = 0;
     this.frameX = 0;
@@ -42,19 +47,21 @@ class Player {
     this.spriteHeight = 327;
   }
 
-  update() {
-    const dx = this.x - mouse.x;
-    const dy = this.y - mouse.y;
-    if (mouse.x != this.x) {
-      this.x -= dx / 20;
+  update(){
+    const dx=  this.x - mouse.x;
+    const dy = this.y - mouse.y; 
+    let theta = Math.atan2(dy, dx);
+    this.angle = theta;
+    if(mouse.x != this.x){
+      this.x-=dx/20;
     }
-    if (mouse.y != this.y) {
-      this.y -= dy / 20;
+    if(mouse.y != this.y){
+      this.y-=dy/20;
     }
   }
 
-  draw() {
-    if (mouse.click) {
+  draw(){
+    if(mouse.click){
       ctx.lineWidth = 0.2;
       ctx.beginPath();
       ctx.moveTo(this.x, this.y);
@@ -63,9 +70,41 @@ class Player {
     }
     ctx.fillStyle = 'red';
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
     ctx.fill();
     ctx.closePath();
+    ctx.fillRect(this.x, this.y, this.radius, 10);
+    
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    if ( this.x >= mouse.x){
+      ctx.drawImage(
+        playerLeft, 
+        this.frameX* this.spriteWidth, 
+        this.frameY * this.spriteHeight,
+        this.spriteWidth, 
+        this.spriteHeight,
+        0- 60,
+        0- 45,
+        this.spriteWidth/4,
+        this.spriteHeight/4 
+      );
+    } else {
+      ctx.drawImage(
+        playerRight, 
+        this.frameX* this.spriteWidth, 
+        this.frameY * this.spriteHeight,
+        this.spriteWidth, 
+        this.spriteHeight,
+        0- 60,
+        0- 45,
+        this.spriteWidth/4,
+        this.spriteHeight/4 
+      );
+    }
+    ctx.restore();
+
   }
 }
 
@@ -73,54 +112,68 @@ const player = new Player();
 
 
 //Bubbles
-const bubblesArray = [];
+const bubblesArray =[];
 class Bubble {
-  constructor() {
+  constructor(){
     this.x = Math.random() * canvas.width;
-    this.y = canvas.height + 100;
+    this.y = canvas.height+ 100;
     this.radius = 50;
-    this.speed = Math.random() * 5 + 1;
+    this.speed = Math.random()*5 +1;
     this.distance;
+    this.counted= false;
+    this.sound = Math.random() <= 0.5 ? 'sound1' : 'sound2';
   }
 
-  update() {
-    this.y -= this.speed;
+  update(){
+    this.y -=this.speed;
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    this.distance = Math.sqrt(dx*dx + dy*dy);
   }
-  draw() {
+  draw(){
     ctx.fillStyle = 'blue';
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
     ctx.fill();
     ctx.closePath();
   }
 }
 
-function handleBubbles() {
-  if (gameFrame % 50 == 0) {
+const bubblePop1 = document.createElement('audio');
+bubblePop1.src = './assets/audio/Plop.ogg';
+const bubblePop2 = document.createElement('audio');
+bubblePop2.src = './assets/audio/Plop.ogg';
+
+function handleBubbles(){
+  if (gameFrame % 50 == 0){
     bubblesArray.push(new Bubble());
-    // console.log(bubblesArray.length);
   }
-  for (let i = 0; i < bubblesArray.length; i++) {
+  for (let i = 0; i< bubblesArray.length; i++){
     bubblesArray[i].update();
     bubblesArray[i].draw();
-
-  }
-  for (let i = 0; i < bubblesArray.length; i++) {
-    if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
+    if(bubblesArray[i].y < 0 - bubblesArray[i].radius *2){
       bubblesArray.splice(i, 1);
-    }
-    // Collision detection
-    if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
-      ``
-      console.log('collision');
+      i--;
+    } else if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius){
+      if(!bubblesArray[i].counted){
+        if(bubblesArray[i].sound == 'sound1'){
+          // bubblePop1.play();
+        } else {
+          // bubblePop2.play();
+        }
+        score++;
+        bubblesArray[i].counted = true;
+        bubblesArray.splice(i, 1);
+        i--;
+      }
     }
   }
 }
 
 
 // Animation loop
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function animate(){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
   handleBubbles();
   player.update();
   player.draw();
@@ -131,3 +184,6 @@ function animate() {
 
 animate();
 
+window.addEventListener('resize', function(){
+   canvasPosition = canvas.getBoundingClientRect();
+})
