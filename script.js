449b2463e1d585e28920954fdb5657ff68d1f7cd
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-04-28 15:09:14 +0200

    final 1.0

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ canvas.height = 500;
 
 let score = 0;
 let gameFrame = 0;
-ctx.font = '50px Georgia';
+ctx.font = '40px Georgia';
+let gameSpeed= 1;
+let gameOver = false;
 
 
 //Mouse Interactivity
@@ -68,12 +70,12 @@ class Player {
       ctx.lineTo(mouse.x, mouse.y);
       ctx.stroke();
     }
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-    ctx.fill();
-    ctx.closePath();
-    ctx.fillRect(this.x, this.y, this.radius, 10);
+    // ctx.fillStyle = 'red';
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+    // ctx.fill();
+    // ctx.closePath();
+    // ctx.fillRect(this.x, this.y, this.radius, 10);
     
     ctx.save();
     ctx.translate(this.x, this.y);
@@ -113,6 +115,8 @@ const player = new Player();
 
 //Bubbles
 const bubblesArray =[];
+const bubbleImage = new Image();
+bubbleImage.src = 'bubble_pop_frame_01.png';
 class Bubble {
   constructor(){
     this.x = Math.random() * canvas.width;
@@ -131,11 +135,18 @@ class Bubble {
     this.distance = Math.sqrt(dx*dx + dy*dy);
   }
   draw(){
-    ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-    ctx.fill();
-    ctx.closePath();
+    // ctx.fillStyle = 'blue';
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+    // ctx.fill();
+    // ctx.closePath();
+    // ctx.stroke();
+    ctx.drawImage(
+      bubbleImage, 
+      this.x-65, 
+      this.y-65, 
+      this.radius*2.6, 
+      this.radius*2.6);
   }
 }
 
@@ -170,16 +181,120 @@ function handleBubbles(){
   }
 }
 
+// Reapeating backgrounds
+const background = new Image();
+background.src = './background1.png';
+
+const BG = {
+  x1:0,
+  x2: canvas.width,
+  y: 0,
+  width: canvas.width,
+  height: canvas.height
+}
+
+function handleBackground(){
+  
+  BG.x1 -= gameSpeed;
+  if(BG.x1 < -BG.width) BG.x1 = BG.width;
+
+  BG.x2 -= gameSpeed;
+  if(BG.x2 < -BG.width) BG.x2 = BG.width;
+
+  ctx.drawImage(background, BG.x1, BG.y, BG.width, BG.height);
+  ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height);
+}
+
+const enemyImage = new Image();
+enemyImage.src = 'enemy1.png';
+
+class Enemy {
+  constructor(){
+    this.x = canvas.width - 200;
+    this.y = Math.random() * (canvas.height - 150) + 90;
+    this.radius = 60;
+    this.speed = Math.random()*2 +2;
+    this.frame = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.spriteWidth = 418;
+    this.spriteHeight = 397;
+  }
+
+  draw(){
+    // ctx.fillStyle = 'red';
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+    // ctx.fill();
+    ctx.drawImage(
+      enemyImage,
+      this.frameX * this.spriteWidth,
+      this.frameY * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x-60, 
+      this.y-70,
+      this.spriteWidth/3, 
+      this.spriteHeight/3
+      )
+  }
+
+  update(){
+    this.x -=this.speed;
+    if(this.x <0 - this.radius *2 ){
+      this.x = canvas.width +200;
+      this.y = Math.random() * (canvas.height -150)+90;
+      this.speed = Math.random() *2 +2;
+    } 
+    if (gameFrame %5 == 0){
+      this.frame++;
+      if (this.frame >= 12) this.frame =0;
+      if (this.frame == 3 || this.frame == 7 || this.frame == 11){
+        this.frameX = 0;
+      } else {
+        this.frameX ++;
+      }
+      if(this.frame <3) this.frameY =0;
+      else if( this.frame < 7) this.frameY = 1;
+      else if(this.frame < 11) this.frameY =2;
+      else this.frameY =0;
+    }
+
+    // Enemy collision
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    const distance = Math.sqrt(dx * dx + dy*dy);
+    if(distance < this.radius + player.radius){
+      handleGameOver();
+    }
+  }
+}
+
+const enemy1 = new Enemy();
+function handleEnemies(){
+  
+  enemy1.draw();
+  enemy1.update();
+}
+
+function handleGameOver(){
+  ctx.fillStyle = 'white';
+  ctx.fillText('GAME OVER, you reached score ' + score, 110, 250);
+  gameOver = true;
+}
 
 // Animation loop
 function animate(){
   ctx.clearRect(0,0, canvas.width, canvas.height);
+  handleBackground();
   handleBubbles();
   player.update();
   player.draw();
+  handleEnemies();
+  ctx.fillStyle = 'black';
   ctx.fillText('score: ' + score, 10, 50);
   gameFrame++;
-  requestAnimationFrame(animate);
+  if(!gameOver) requestAnimationFrame(animate);
 }
 
 animate();
